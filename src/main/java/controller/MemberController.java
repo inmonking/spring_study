7260b867d0841911a01e25abc4946c1bcbf5e2f0
190302D\USER <sogt728@naver.com>
2020-08-01 20:36:56 +0900

    테스트3

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -13,6 +13,7 @@ public class MemberController {
 	public ModelAndView test() {
 		System.out.println("테스트");
 		System.out.println("테스트2");
+		System.out.println("테스트3");
 		mv.setViewName("member/test");
 		return mv;
 	}
