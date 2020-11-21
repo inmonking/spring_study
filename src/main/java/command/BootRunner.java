cb497ae86ebc55536d7215428d7b6f814b5f4274
DESKTOP-IEGTRSN\Choi InJun <inmonking@gmail.com>
2020-11-21 21:17:18 +0900

    postgres -> oracle

--- src/main/java/command/BootRunner.java
+++ src/main/java/command/BootRunner.java
@@ -27,34 +27,35 @@ public class BootRunner {
 			String tableName = "team_member";
 			if((int)sqlSession.selectOne(namespace + ".findTable",tableName)<=0) {
 				Map<String, String> map = new HashMap<String, String>();
-				sql = "CREATE TABLE public.team_member (\r\n" + 
-						"	id varchar(15) NOT NULL,\r\n" + 
-						"	\"password\" varchar(25) NOT NULL,\r\n" + 
-						"	gender varchar(2) NOT NULL,\r\n" + 
-						"	\"name\" varchar(20) NOT NULL,\r\n" + 
-						"	email varchar(40) NOT NULL,\r\n" + 
-						"	phone varchar(11) NOT NULL,\r\n" + 
-						"	CONSTRAINT team_member_pkey PRIMARY KEY (id)\r\n" + 
-						");";
+				sql = "CREATE TABLE \"TEAM_MEMBER\" \r\n" + 
+						"   (	\"ID\" VARCHAR2(15) NOT NULL ENABLE, \r\n" + 
+						"	\"password\" VARCHAR2(25) NOT NULL ENABLE, \r\n" + 
+						"	\"GENDER\" VARCHAR2(2) NOT NULL ENABLE, \r\n" + 
+						"	\"name\" VARCHAR2(20) NOT NULL ENABLE, \r\n" + 
+						"	\"EMAIL\" VARCHAR2(40) NOT NULL ENABLE, \r\n" + 
+						"	\"PHONE\" VARCHAR2(11) NOT NULL ENABLE, \r\n" + 
+						"	 CONSTRAINT \"TEAM_MEMBER_PKEY\" PRIMARY KEY (\"ID\")\r\n" + 
+						"	 )";
 				map.put("create_query", sql);
 				sqlSession.selectOne(namespace +".createQuery", map);
 			}
 			tableName = "team_qna";
 			if((int)sqlSession.selectOne(namespace + ".findTable",tableName)<=0) {
 				Map<String, String> map = new HashMap<String, String>();
-				sql = "CREATE TABLE public.team_qna (\r\n" + 
-						"	num serial primary key,\r\n" + 
-						"	cat_name varchar(20) NOT NULL,\r\n" + 
-						"	writer varchar(10) NOT NULL,\r\n" + 
-						"	subject varchar(50) NOT NULL,\r\n" + 
-						"	\"password\" varchar(12) NOT NULL,\r\n" + 
-						"	reg_date date NOT NULL,\r\n" + 
-						"	readcount int8 NULL DEFAULT 0,\r\n" + 
-						"	\"ref\" int8 NOT NULL,\r\n" + 
-						"	re_step int8 NOT NULL,\r\n" + 
-						"	re_level int8 NOT NULL,\r\n" + 
-						"	\"content\" varchar(3000) NOT NULL\r\n" + 
-						");";
+				sql = "CREATE TABLE \"TEAM_QNA\" \r\n" + 
+						"   (	\"NUM\" NUMBER(*,0), \r\n" + 
+						"	\"CAT_NAME\" VARCHAR2(20) NOT NULL ENABLE, \r\n" + 
+						"	\"WRITER\" VARCHAR2(10) NOT NULL ENABLE, \r\n" + 
+						"	\"SUBJECT\" VARCHAR2(50) NOT NULL ENABLE, \r\n" + 
+						"	\"password\" VARCHAR2(12) NOT NULL ENABLE, \r\n" + 
+						"	\"REG_DATE\" DATE NOT NULL ENABLE, \r\n" + 
+						"	\"READCOUNT\" NUMBER(*,0), \r\n" + 
+						"	\"ref\" NUMBER(*,0) NOT NULL ENABLE, \r\n" + 
+						"	\"RE_STEP\" NUMBER(*,0) NOT NULL ENABLE, \r\n" + 
+						"	\"RE_LEVEL\" NUMBER(*,0) NOT NULL ENABLE, \r\n" + 
+						"	\"content\" VARCHAR2(3000) NOT NULL ENABLE, \r\n" + 
+						"	 PRIMARY KEY (\"NUM\")\r\n" + 
+						"	 )";
 				map.put("create_query", sql);
 				sqlSession.selectOne(namespace +".createQuery", map);
 			}
